5e215528d6bccb41f3eb4b07bcc59690ba181cf5
ezekiellepcha19-pixel <ezekiellepcha19@gmail.com>
2026-02-14 01:03:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,10 +78,10 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dus9zlpmm/video/upload/v1771011097/New_West_-_Those_Eyes_Lyrics_-_Dan_Music_wmkfae.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
